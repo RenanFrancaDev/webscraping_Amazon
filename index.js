c5f8bafc11ce6c67513a2217451c4cd7c739aeb2
Renan Franca <renanfranca.dev@gmail.com>
2023-11-10 18:49:53 -0300

    test: getPageItems working correctly

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
-const filesystem = require('fs')
 const browserHeaders = require('./src/object/browserHeaders')
 const slug = require('./src/helpers/slug')
-const readFromFile = require('./src/functions/readFromFile')
-const writeToFile = require('./src/functions/writeFromFile')
+// const readFromFile = require('./readFromFile')
+// const writeToFile = require('./writeFromFile')
+const filesystem = require('fs')
 
 
 const BASE_URL = 'https://www.amazon.com.br/'
-const path = 's?k=carro&page=0'
+const path = 's?k=carro&page=1'
 
 const getPage = () => {
 
@@ -18,6 +18,35 @@ const getPage = () => {
     };
 
     return axios.get(url, options).then((response) => response.data)
+}
+
+//verify if the consult alreay exist
+const readFromFile = (filename) => {
+    const promiseCallback = (resolve, reject) => {
+        filesystem.readFile(filename, 'utf8', (error, contents) => {
+            if(error) {
+            console.log('there is not cache')
+            resolve(null);
+            }
+            resolve(contents)
+        })
+    }
+    return new Promise(promiseCallback)
+}
+
+//FUNCTION TO CREATE FILE HTML
+const writeToFile = (data, path) => {
+
+    const promiseCallback = (resolve, reject) => {
+        filesystem.writeFile(path, data, (error) => {
+            if(error){
+                reject(error);
+                return
+            }
+            resolve(true)
+        })
+    }
+    return new Promise(promiseCallback);
 }
 
 //GET QUERY READY, INF NOT, IT WILL CREATE
@@ -42,6 +71,29 @@ const getCachedPage = (path) => {
     return new Promise(promiseCallback)
 }
 
+const getPageItems = (html) => {
 
-getCachedPage(path).then(console.log).catch(console.error);
+    const $ = cheerio.load(html);
+
+    const promiseCallback = (resolve, reject) =>{
+
+        objects = []
+        const results = $('[data-component-type=s-search-result]');
+        results.each((index, results) => {
+            objects[index] = {
+                title: $(results).find('a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal span.a-color-base.a-text-normal').text()
+                
+            }
+        })
+        console.log(objects)
+
+        resolve(true)
+    }
+
+    return new Promise(promiseCallback)
+}
+
+
+getCachedPage(path).then(getPageItems).then(console.log).catch(console.error);
+
 
