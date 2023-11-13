7a3ccd34a88721fa95a855e7e120bcf5afe5c432
Renan Franca <renanfranca.dev@gmail.com>
2023-11-13 14:40:40 -0300

    fix: ensure that search already was done

--- src/functions/scrape.js
+++ src/functions/scrape.js
@@ -78,4 +78,4 @@ const getPageItems = (html) => {
 
 getCachedPage(path).then(getPageItems).then(console.log).catch(console.error);
 
-module.exports = getPageItems;
+module.exports = getCachedPage;
